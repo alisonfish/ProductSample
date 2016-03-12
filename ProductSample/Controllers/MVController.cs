605c8f25831eb322abd7301311b5bd7268c578a2
alison <s250376@hotmail.com>
2016-03-12 15:40:12 +0800

    Practice 29

---
+++ ProductSample/Controllers/MVController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductSample.Controllers
+{
+    public class MVController : Controller
+    {
+        // GET: MV
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Name, DateTime Birthday)
+        {
+            return Content(Name + " " + Birthday);
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(FormCollection form)
+        //{
+        //    return Content(form["Name"] + " " + form["Birthday"]);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Index(int a)
+        //{
+        //    return Content(Request.Form["Name"] + " " + Request.Form["Birthday"]);
+        //}
+    }
+}
