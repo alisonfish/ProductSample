ae80506cda3ad912c671b197d7068c02cf04d95e
alison <s250376@hotmail.com>
2016-03-13 17:28:15 +0800

    Practice @Html.Partial and @Html.Action

--- ProductSample/Controllers/ProductController.cs
+++ ProductSample/Controllers/ProductController.cs
@@ -15,13 +15,19 @@ namespace ProductSample.Controllers
         //ProductRepository repo = RepositoryHelper.GetProductRepository();
 
         // GET: Product
-        public ActionResult Index()
+        public ActionResult Index(int? ProductId, string type)
         {
             var data = repo.All().Take(5);
 
             //var data = repo.Get超級複雜的資料集();
 
             //var repoOL = RepositoryHelper.GetOrderLineRepository(repo.UnitOfWork);
+            ViewBag.type = type;
+
+            if (ProductId.HasValue)
+            {
+                ViewBag.SelectedProductId = ProductId.Value;
+            }
 
             return View(data);
         }
