3cfff6b6d85432d86b1bdd2bb794de6f825c2da1
alison <s250376@hotmail.com>
2016-03-13 11:35:39 +0800

    Practice HandleError

--- ProductSample/Controllers/HomeController.cs
+++ ProductSample/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using ProductSample.ActionFilters;
+using System;
+using System.Data.SqlClient;
 using System.Web.Mvc;
 
 namespace ProductSample.Controllers
@@ -24,6 +26,24 @@ namespace ProductSample.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        //指定不同的ExceptionType顯示不同的View
+        [HandleError(ExceptionType = typeof(ArgumentException), View ="ErrorArgument")]
+        [HandleError(ExceptionType = typeof(SqlException), View = "ErrorSQL")]
+        public ActionResult ErrorTest(string errorType)
+        {
+            if (errorType == "1")
+            {
+                throw new Exception("Error 1");
+            }
+
+            if (errorType == "2")
+            {
+                throw new ArgumentException("Error 2");
+            }
+
+            return Content("No Error");
         }
     }
 }
